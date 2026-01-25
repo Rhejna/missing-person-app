12dced48a54c9fedc55561a0249a268c9a8d8f0f
Rhejna <vannrheez@gmail.com>
2026-01-25 23:11:06 +0100

    allow frontend access

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# CORS configuration
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # allow GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"],  # allow all headers
+)
+
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"status": "backend is running"}
